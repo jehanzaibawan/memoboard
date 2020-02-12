044d53e3842168186ebeacf5230d1cd1af80877e
jehanzaib <jehanzaib_awan@hotmail.com>
2020-02-12 17:47:57 +0400

    add styled components, create basic folder structure

---
+++ src/pages/Ideas.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const Ideas = (): ReactElement => {
+  return <Wrapper>Hello World</Wrapper>;
+};
+
+export default Ideas;
